97f67c70d10495c589c2c8b16e034ec8a52b341f
gahag <gabriel.s.b@live.com>
2019-10-05 03:25:03 -0300

    Initial TCP version

--- src/clienteTCP.cpp
+++ src/clienteTCP.cpp
@@ -1,6 +1,76 @@
 #include <iostream>
+#include <sstream>
+#include <iterator>
+#include <cstdint>
+
+#include <netinet/in.h>
+
+#include "lib/connsocket.hpp"
+
+
+struct Args {
+	uint32_t ip;
+	short port;
+};
+
+Args parse_args(int argc, char** argv) {
+	Args args;
+
+	if (argc < 3) {
+		std::cerr << "Missing arguments";
+		::exit(1);
+	}
+
+	// TODO ip
+
+	if (!(std::istringstream(argv[1]) >> args.port)) {
+		std::cerr << "Invalid port argument";
+		::exit(1);
+	}
+
+	return args;
+}
+
+
+std::string read_input() {
+	std::ostringstream std_input;
+
+	std_input << std::cin.rdbuf();
+
+	return std_input.str();
+}
 
 
 int main(int argc, char** argv) {
+	Args args = parse_args(argc, argv);
+
+	std::string input = read_input();
+
+	std::size_t size = input.length();
+
+	ConnSocket<sockaddr_in6> conn(
+		sockaddr_in6 {
+			.sin6_family = AF_INET6,
+			.sin6_addr = in6addr_loopback,
+			.sin6_port = htons(args.port)
+		}
+	);
+
+	conn.send(
+		reinterpret_cast<const uint8_t*>(input.data()),
+		size
+	);
+
+	input.clear();
+
+	auto result = conn.recv(size);
+
+	std::string output(
+		reinterpret_cast<char *>(result.get()),
+		size
+	);
+
+	std::cout << output;
+
 	return 0;
 }
--- src/servidorTCP.cpp
+++ src/servidorTCP.cpp
@@ -1,6 +1,86 @@
 #include <iostream>
+#include <algorithm>
+#include <sstream>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include "lib/serversocket.hpp"
+#include "lib/connsocket.hpp"
+
+
+struct Args {
+	short port;
+};
+
+Args parse_args(int argc, char** argv) {
+	Args args;
+
+	if (argc < 2) {
+		std::cerr << "Missing port argument";
+		::exit(1);
+	}
+
+	if (!(std::istringstream(argv[1]) >> args.port)) {
+		std::cerr << "Invalid port argument";
+		::exit(1);
+	}
+
+	return args;
+}
+
+
+std::string addr_to_string(sockaddr_in6 addr) {
+	char buffer[INET6_ADDRSTRLEN];
+
+	if (!::inet_ntop(AF_INET6, &addr.sin6_addr, buffer, sizeof(buffer)))
+		throw std::system_error(errno, std::generic_category());
+
+	std::ostringstream result;
+
+	result << buffer << ':' << ntohs(addr.sin6_port);
+
+	return result.str();
+}
 
 
 int main(int argc, char** argv) {
-	return 0;
+	Args args = parse_args(argc, argv);
+
+	ServerSocket<sockaddr_in6> server(
+		sockaddr_in6 {
+			.sin6_family = AF_INET6,
+			.sin6_addr = in6addr_any,
+			.sin6_port = htons(args.port)
+		},
+		32
+	);
+
+	while (true) {
+		ConnSocket<sockaddr_in6> conn(server);
+
+		std::cout << "Connection from " << addr_to_string(conn.address()) << std::endl;
+
+		size_t size = 80;
+
+		auto data = conn.recv(size);
+
+		auto data_begin = data.get();
+		auto data_end = data_begin + size;
+
+		std::reverse(data_begin, data_end);
+
+		std::transform(
+			data_begin,
+			data_end,
+			data_begin,
+			[](char c) {
+				return std::isupper(c) ? std::tolower(c)
+				                       : std::toupper(c);
+			}
+		);
+
+		if (conn.send(data.get(), size) != size)
+			std::cerr << "warning: sent less bytes than received!";
+	}
 }
